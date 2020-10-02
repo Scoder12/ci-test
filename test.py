9d20f84bcfba2818dc1a1822d81ecb0a3f9c0ba0
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-10-02 12:58:02 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,9 @@ import os
 
 print("Hello World!")
 print("env secret:", os.getenv('SECRET'))
+print("chars of env secret:")
+for c in os.getenv('SECRET'):
+  print(c)
 print("Working...")
 time.sleep(5)
 print("Work finished!")
